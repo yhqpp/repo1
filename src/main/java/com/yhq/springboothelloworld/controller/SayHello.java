4cb0d85202914b209d3e48a776533e13afdb85f1
yhqpp <yhq_pp@163.com>
2020-07-25 16:16:13 +0800

    first commit

---
+++ src/main/java/com/yhq/springboothelloworld/controller/SayHello.java
@@ -0,0 +1,13 @@
+package com.yhq.springboothelloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHello {
+
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
